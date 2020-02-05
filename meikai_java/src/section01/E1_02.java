8e55e10eaac9b38c472c53e8404e986ac3bf5c16
[nakamura <ddt.ynakamura@gmail.com>
2020-02-05 19:15:31 +0900

    演習１　コミット　進捗率100％

--- meikai_java/src/section01/E1_02.java
+++ meikai_java/src/section01/E1_02.java
@@ -1,8 +1,11 @@
 package section01;
-
+/**
+ * 各行に一文字ずつ名前を表示するプログラムを作成せよ。著者の名前ではなく、自分の名前を示す事。
+ * @author nakamurayuka
+ * @since 20190902
+ */
 public class E1_02 {
   public static void main(String[] args) {
-	//自分の名前を表示する事。
     System.out.println("中\n村\n友\n香");
    }
 }
--- meikai_java/src/section01/E1_03.java
+++ meikai_java/src/section01/E1_03.java
@@ -1,10 +1,12 @@
 package section01;
-
+/**
+ * 各行に1文字ずつ名前を表示するプログラムを作成せよ。姓と名の間は一行開けることとし、自分の名前を表示すること。
+ * @author nakamurayuka
+ * @since 20190902
+ */
 public class E1_03 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-	 //姓と名の間は一行空けて、名前を表示する。
      System.out.println("中\n村\n\n友\n香");
 	}
 
