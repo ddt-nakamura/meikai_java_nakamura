42d8027112d28a64305679320f4a3cb84c7dcd72
[nakamura <ddt.ynakamura@gmail.com>
2020-02-12 21:26:11 +0900

    演習3-1~2を一時的にコミットします　進捗率50%

--- meikai_java/src/section03/E3_01.java
+++ meikai_java/src/section03/E3_01.java
@@ -1,4 +1,7 @@
 package section03;
+
+import java.util.Scanner;
+
 /**
  * 右に示すように、整数値を読み込んで、その絶対値を求めて表示するプログラムを作成せよ。
  * @author nakamurayuka
@@ -8,8 +11,20 @@ package section03;
 public class E3_01 {
 
 	public static void main(String[] args) {
-		
-
+		 //キーボードに入力された文字を出力する
+        Scanner stdIn= new Scanner(System.in);
+        
+        //「整数値:」をコンソール画面に出力
+		System.out.print("整数値:");
+		//文字列を読み込む
+        int number1  = stdIn.nextInt();
+        
+        //もしnumber1が0より大きければtrueの処理を行う
+        if(number1 < 0) {
+        	//もしnumber1の値が-のとき、-がなくなるように-1にする、
+        	number1 = number1*-1;
+        }
+        //trueのとき「その絶対値は5です。」をコンソール画面に出力
+        System.out.println("その絶対値は" + number1 +"です。");
 	}
-
 }
---
+++ meikai_java/src/section03/E3_02.java
@@ -0,0 +1,16 @@
+package section03;
+
+/**
+ * 右に示すように、整数値を読み込んで、その絶対値を求めて表示するプログラムを作成せよ。
+ * @author nakamurayuka
+ * @since  20200212
+ */
+
+public class E3_02 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
