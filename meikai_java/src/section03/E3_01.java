36a1d2ebeb503050b4bbfa4937f55ebd00bcadd7
[nakamura <ddt.ynakamura@gmail.com>
2020-02-12 20:12:32 +0900

    演習３_０１　修正のためコミット　進捗率10%

---
+++ meikai_java/src/section03/E3_01.java
@@ -0,0 +1,16 @@
+package section03;
+
+/**
+ * 右に示すように、整数値をよみこんで、その絶対値を求めて表示するプログラムを作成せよ。
+ * 
+ * @author nakamurayuka
+ * @since  20200212
+ */
+
+public class E3_01 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
