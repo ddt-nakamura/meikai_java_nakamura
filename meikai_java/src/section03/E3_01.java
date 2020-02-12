03d153505ef109276affb85be3c9e75f6caf694c
[nakamura <ddt.ynakamura@gmail.com>
2020-02-12 20:19:59 +0900

    ブランチ切り替えのためコミットします。

--- meikai_java/src/section03/E3_01.java
+++ meikai_java/src/section03/E3_01.java
@@ -1,9 +1,14 @@
 package section03;
+/**
+ * 右に示すように、整数値を読み込んで、その絶対値を求めて表示するプログラムを作成せよ。
+ * @author nakamurayuka
+ * @since  20200212
+ */
 
 public class E3_01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		
 
 	}
 
